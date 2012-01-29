fe06192c7306b6eb59d1ce9613dcac2e56f03ef9
Iván Loire <ivan@iloire.com>
2012-01-29 19:59:59 +0100

    refactor

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-var app = require ('./app')
+var config = require ('./config').values
+var redis = require("redis").createClient(config.server.database.port, config.server.database.host);
+
+var app = require ('./app').getApp(redis)
 
 var port = parseInt(process.argv[2], 10) || 3000
 app.listen(port);
@@ -6,6 +9,8 @@ app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 process.on('SIGINT', function () {
+	app.close();
+	redis.quit();
 	console.log();
 	console.log('Shuting down server..');
 	process.exit(0);
