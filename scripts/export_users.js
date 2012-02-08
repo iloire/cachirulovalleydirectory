3376f9296f194a22449ca1e851215e7d452fa2b5
Iván Loire <ivan@iloire.com>
2012-02-08 09:14:51 +0100

    fix config, exit gracefully, by cat

--- scripts/export_users.js
+++ scripts/export_users.js
@@ -1,20 +1,20 @@
 var _redis = require("redis")
 var config = require ('../config').values
-var redis = _redis.createClient(config.server.database.port, config.server.database.host)
+var redis = _redis.createClient(config.server.production.database.port, config.server.production.database.host)
 var common = require ('../lib/common.js');
 
 var module_users = require("../lib/modules/users.js")
 
-var params = { max: 5000 }
-module_users.GetUsers(redis, params, function(data){
+var params = { id_cat: parseInt(process.argv[2], 10) || 1, max: 5000 }
+module_users.GetUsers(redis, params, function(err, users){
 	var fs = require('fs');
-	fs.writeFile("users.json", JSON.stringify(data), function(err) {
+	fs.writeFile("exported_users_cat_" +  params.id_cat + ".json", JSON.stringify(users), function(err) {
 	    if(err) {
-	        console.log(err);
+	        console.error(err);
 	    } else {
-	        console.log("The file was saved!");
+	        console.log("Users from category " + params.id_cat + " saved on file!");
 	    }
+		redis.quit();
+		process.exit(0);
 	});
 })
-
-redis.quit
