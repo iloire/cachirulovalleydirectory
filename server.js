db7fde35baa4dad96ee9abc81ad4452dfb44c3e6
Ivan Loire <iloire@atlassian.com>
2014-07-06 17:46:31 +1000

    Check for missing config values. Fill mssing ;

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-var config = require ('./config').values
+var config = require ('./config').values;
+
+if (!config.base_url || !config.LINKEDIN_API_KEY || !config.LINKEDIN_SECRET_KEY){
+  return console.error("Missing config parameter. Please setup your ENV accordingly");
+}
+
 var redis = require("redis").createClient(config.server.production.database.port, config.server.production.database.host);
 redis.select (config.server.production.database.db);
 
-var app = require ('./app').getApp(redis, config)
+var app = require ('./app').getApp(redis, config);
 
-var port = parseInt(process.argv[2], 10) || 3000
+var port = parseInt(process.argv[2], 10) || 3000;
 app.listen(port);
 
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
